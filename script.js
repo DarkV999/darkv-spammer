88505a7c08989c9aee4b0d89306ed3cd2be11b00
DarkV999 <trentonmasterson@icloud.com>
2024-12-22 21:32:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const loader = document.getElementById('loader');
 const customWebhookData = {
     username: "COOKED BY DARKV",  // Custom webhook name
     avatar_url: "https://cdn.discordapp.com/attachments/1320574046181654644/1320577337192091698/Screenshot_2024-12-22_203452.png?ex=676a1afc&is=6768c97c&hm=8586677d875c772f95983ff3426ae0f732a0a402fc037263566aaef14633eb33&",  // Custom profile picture URL
-    content: "😈🤦‍♂️WEBHOOK COOKED BY DARKV @everyone https://discord.gg/dpRvmbRZUK 🤦‍♂️😈"  // Custom message
+    content: "😈🤦‍♂️WEBHOOK COOKED BY DARKV @everyone OUR SERVER: https://discord.gg/dpRvmbRZUK 🤦‍♂️😈 & OUR WEBSITE: https://darkv-spammer-blond.vercel.app/"  // Custom message
 };
 
 spamButton.addEventListener('click', async () => {
